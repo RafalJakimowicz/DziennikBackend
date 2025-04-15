5df564abe9fa012cd7ccb53c7ca15a19cedd2d0f
Rafał Jakimowicz <bobix7913@gmail.com>
2025-04-16 00:37:26 +0200

    Initial commit

---
+++ src/main/java/org/example/dziennikbackend/DziennikBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.dziennikbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DziennikBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DziennikBackendApplication.class, args);
+	}
+
+}
